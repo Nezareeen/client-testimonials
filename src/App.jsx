86473408829565ab775443a18f3aae4100e1326c
Nezareeen <nezareeen@gmail.com>
2025-04-28 14:46:37 +0530

    Assessment done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import TestimonialCard from './components/TestimonialCard'
+
+function App() {
+
+  return (
+    <>
+      <TestimonialCard/>
+      <br />                            
+      <TestimonialCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TestimonialCard = () => {
+  return (
+    <div>
+        <h3>Emily Watson</h3>
+        <p>This service was amazing! Highly recommended.</p>
+        <br />
+        <h3>Jack Logan</h3>
+        <p>Great customer care and service!</p>
+    </div>
+  )
+}
+
+export default TestimonialCard
